656c8ef7450f7764737f84b9371ea626596a9725
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-07 20:38:25 +0100

    Added the navigation bar and the two components: home, categories

---
+++ src/BookPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './components/Nav';
+
+function BookPage() {
+  return (
+    <>
+      <Nav />
+      <div>BookPage</div>
+    </>
+  );
+}
+
+export default BookPage;
