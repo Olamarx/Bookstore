8bd45835f2447356e2bc0300cbb1bc121ab36ba1
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-13 03:08:20 +0100

    added provider to the index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import theStore from './redux/configStore';
 import App from './App';
 
 const append = document.querySelector('#root');
@@ -9,7 +11,9 @@ const root = createRoot(append);
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={theStore}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
 );
