cc0bdbf04cd2e726f1a450fb5b873160c845a3d8
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-13 03:02:00 +0100

    Addes the categories button

--- src/CategoriesPage.js
+++ src/CategoriesPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Nav from './components/Nav';
+import { Category } from './redux/categories/categories';
 
 function CategoriesPage() {
+  const dispatch = useDispatch();
+
+  const position = useSelector((state) => state.categories);
   return (
     <>
       <Nav />
-      <div>CategoriesPage</div>
+      <div>Categories</div>
+      <button type="button" onClick={() => dispatch(Category())}>
+        Status
+      </button>
+      <p>{position}</p>
     </>
   );
 }
