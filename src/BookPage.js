e706dcb9e7cc41799452405260dfbd0decb4fd8e
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-13 22:16:40 +0100

    Refactored the book page to accept from the API

--- src/BookPage.js
+++ src/BookPage.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import Nav from './components/Nav';
 import Percentage from './components/BookComponent/Percentage';
 import style from './BookPage.module.css';
 import EachBook from './components/BookComponent/eachBook';
 import AddBook from './components/BookComponent/AddBook';
+import { booksFromAPI } from './redux/books/books';
 
 function BookPage() {
   const books = useSelector((state) => state.Books, shallowEqual);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(booksFromAPI());
+  }, []);
   return (
     <>
       <Nav />
@@ -18,8 +23,8 @@ function BookPage() {
               topic={book.topic}
               author={book.author}
               title={book.title}
-              key={book.id}
-              id={book.id}
+              key={book.item_id}
+              id={book.item_id}
               category={book.category}
             />
           ))}
