1a137caf34c73fff0e3d01641554eadae3d4f673
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-13 03:01:28 +0100

    Finished the project

--- src/BookPage.js
+++ src/BookPage.js
@@ -1,21 +1,30 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
 import Nav from './components/Nav';
-import eachBook from './components/BookComponent/eachBook';
 import Percentage from './components/BookComponent/Percentage';
 import style from './BookPage.module.css';
+import EachBook from './components/BookComponent/eachBook';
 import AddBook from './components/BookComponent/AddBook';
 
 function BookPage() {
+  const books = useSelector((state) => state.Books, shallowEqual);
   return (
     <>
       <Nav />
       <div className={style.body}>
         <div className={style.action}>
-          <eachBook
-            userName="Olaoluwa Owoeye"
-            gameName="Sokudo"
-            category="Moon"
-          />
+          {books.map((book) => (
+            <EachBook
+              topic={book.topic}
+              author={book.author}
+              title={book.title}
+              key={book.id}
+              id={book.id}
+              category={book.category}
+            />
+          ))}
+
           <Percentage />
         </div>
       </div>
